7388017cc4ccf85edb5cdf47a9e25ac9a6a0ec9b
Claire Watson <watson.e.claire@gmail.com>
2018-06-11 14:37:36 -0400

    bubble sort initial commit

---
+++ bubblesort.spec.js
@@ -0,0 +1,11 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect(bubbleSort([])).toEqual( [] );
+  });
+
+  it('sorts numbers in arrays', function(){
+    expect(bubbleSort( [9, 5, 2, 4, 3] )).toEqual( [2, 3, 4, 5, 9] );
+    expect(bubbleSort( [3, 1, 5, 2, 4] )).toEqual( [1, 2, 3, 4, 5] );
+    expect(bubbleSort( [1, 2, 3, 4, 5] )).toEqual( [1, 2, 3, 4, 5] );
+  });
+});
